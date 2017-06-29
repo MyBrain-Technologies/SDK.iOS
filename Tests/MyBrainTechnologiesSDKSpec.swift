20a76a0697ee8a0be6bcfb4bb3ba8e671acd960f
B_Rass <baptiste.rasschaert@gmail.com>
2017-06-29 16:23:55 +0200

    Clean Test file

--- Tests/MyBrainTechnologiesSDKSpec.swift
+++ Tests/MyBrainTechnologiesSDKSpec.swift
@@ -5,22 +5,3 @@
 //  Created by Baptiste Rasschaert on 04/10/16.
 //  Copyright © 2017 MyBrainTechnologies. All rights reserved.
 //
-
-import Quick
-import Nimble
-@testable import MyBrainTechnologiesSDK
-
-class MyBrainTechnologiesSDKSpec: QuickSpec {
-
-    override func spec() {
-
-        describe("MyBrainTechnologiesSDKSpec") {
-            it("works") {
-                expect("MyBrainTechnologiesSDK") == "MyBrainTechnologiesSDK"
-            }
-            
-        }
-
-    }
-
-}
