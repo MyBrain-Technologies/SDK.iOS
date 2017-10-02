ce0d12e9e7feb59c56c3d57d414402726af78534
B_Rass <baptiste.rasschaert@gmail.com>
2017-10-02 13:01:28 +0200

    Add File for Big big Bridge constants

---
+++ Sources/Utils/CPPSignalProcessing/Code bridge/MBTBridgeConstants.h
@@ -0,0 +1,29 @@
+//
+//  MBTBridgeConstants.h
+//  MyBrainTechnologiesSDK
+//
+//  Created by Baptiste Rasschaert on 02/10/2017.
+//  Copyright © 2017 MyBrainTechnologies. All rights reserved.
+//
+
+#ifndef MBTBridgeConstants_h
+#define MBTBridgeConstants_h
+
+#import "MBT_Matrix.h"
+
+extern const std::vector<float> trainingFeatureList;
+extern const MBT_Matrix<float> trainingFeatures;
+extern const std::vector<float> trainingClasses;
+extern const std::vector<float> cleanItakuraDistance;
+
+extern const std::vector<float> spectrumClean;
+
+extern const std::vector<float> sigma;
+
+extern const std::vector<float> mu;
+
+extern const std::vector<float> w;
+
+
+
+#endif /* MBTBridgeConstants_h */
