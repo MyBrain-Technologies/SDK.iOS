11af43b6e0d41fa6c729ce2511e2ad73930a56bc
Rochdi Aries <facileit@starfruit.local>
2018-07-13 16:09:50 +0200

    - PATCH : update file project

---
+++ MyBrainTechnologiesSDK/MyBrainTechnologiesSDK.h
@@ -0,0 +1,19 @@
+//
+//  MyBrainTechnologiesSDK.h
+//  MyBrainTechnologiesSDK
+//
+//  Created by facileit on 13/07/2018.
+//  Copyright © 2018 MyBrainTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyBrainTechnologiesSDK.
+FOUNDATION_EXPORT double MyBrainTechnologiesSDKVersionNumber;
+
+//! Project version string for MyBrainTechnologiesSDK.
+FOUNDATION_EXPORT const unsigned char MyBrainTechnologiesSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyBrainTechnologiesSDK/PublicHeader.h>
+
+
