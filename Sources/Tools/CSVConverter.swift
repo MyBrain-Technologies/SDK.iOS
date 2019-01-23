1023c73b062e44cfe4979e4177c792c3f27b9ab2
Mathilde RESSIER <ressier@stg-interactive.com>
2019-01-23 18:27:23 +0100

    some clean

---
+++ Sources/Tools/CSVConverter.swift
@@ -0,0 +1,9 @@
+//
+//  String+extension.swift
+//  MyBrainTechnologiesSDK-iOS
+//
+//  Created by Mathilde on 23/01/2019.
+//  Copyright © 2019 MyBrainTechnologies. All rights reserved.
+//
+
+import Foundation
