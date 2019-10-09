1b7a33fb7ad1975290c8c7a6ee84b5fea858ef94
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2019-10-09 14:49:28 +0200

    Code formatting

--- Sources/Client/MelomindEngineDelegate.swift
+++ Sources/Client/MelomindEngineDelegate.swift
@@ -1,15 +1,13 @@
-//
-//  MelomindEngineDelegate.swift
-//  MyBrainTechnologiesSDK
-//
-//  Created by Baptiste Rasschaert on 26/06/2017.
-//  Copyright © 2017 MyBrainTechnologies. All rights reserved.
-//
-
 import Foundation
 
-/// General delegate of the SDK for MelomindEngine.
-/// Get data or information from the Headset out the SDK.
+/*******************************************************************************
+ * MelomindEngineDelegate
+ *
+ * General delegate of the SDK for MelomindEngine.
+ * Get data or information from the Headset out the SDK.
+ *
+ ******************************************************************************/
+
 public protocol MelomindEngineDelegate:
   MBTBluetoothEventDelegate,
   MBTBluetoothA2DPDelegate,
--- Sources/Tools/CSVConverter.swift
+++ Sources/Tools/CSVConverter.swift
@@ -9,25 +9,42 @@
 import Foundation
 
 class CSVConverter {
-    static public func data(fromFile filename: String, lineSeparator: String, columnSeparator: String)  -> [[String]] {
-        guard let content = try? String(contentsOfFile: filename, encoding: .utf8) else { return [[String]]() }
-        _ = clearInput(content)
-        return CSVConverter.data(fromString:content, lineSeparator: lineSeparator, columnSeparator: columnSeparator)
+
+  static public func data(fromFile filename: String,
+                          lineSeparator: String,
+                          columnSeparator: String)  -> [[String]] {
+    guard let content = try? String(contentsOfFile: filename, encoding: .utf8)
+      else {
+        return [[String]]()
     }
-    
-    static public func data(fromString string: String, lineSeparator: String, columnSeparator: String) -> [[String]] {
-        let clearContent = clearInput(string)
-        let lines = clearContent.components(separatedBy: lineSeparator)
-        var data = [[String]]()
-        for line in lines {
-            if !line.isEmpty {
-                data.append(line.components(separatedBy: columnSeparator))
-            }
-        }
-        return data
+
+    clearInput(content)
+
+    return CSVConverter.data(fromString:content,
+                             lineSeparator: lineSeparator,
+                             columnSeparator: columnSeparator)
+  }
+
+  static public func data(fromString string: String,
+                          lineSeparator: String,
+                          columnSeparator: String) -> [[String]] {
+    let clearContent = clearInput(string)
+
+    let lines = clearContent.components(separatedBy: lineSeparator)
+    var data = [[String]]()
+
+    for line in lines {
+      if !line.isEmpty {
+        data.append(line.components(separatedBy: columnSeparator))
+      }
     }
-    
-    static private func clearInput(_ string: String) -> String {
-        return string.replacingOccurrences(of: "\r", with: "")
-    }
+
+    return data
+  }
+
+  @discardableResult
+  static private func clearInput(_ string: String) -> String {
+    return string.replacingOccurrences(of: "\r", with: "")
+  }
+
 }
