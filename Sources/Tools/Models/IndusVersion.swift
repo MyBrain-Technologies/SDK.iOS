3b8b1df2efa329fe4aee1419555f0f396fc24d6f
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-01-13 18:56:41 +0100

    Add new firmware versions

---
+++ Sources/Tools/Models/IndusVersion.swift
@@ -0,0 +1,9 @@
+//
+//  IndusVersion.swift
+//  MyBrainTechnologiesSDK-iOS
+//
+//  Created by Mathilde Ressier on 13/01/2020.
+//  Copyright © 2020 MyBrainTechnologies. All rights reserved.
+//
+
+import Foundation
