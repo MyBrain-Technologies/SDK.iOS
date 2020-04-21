0b11b3db35906210ebc5202fd211c943009f5465
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-04-21 14:04:26 +0200

    Use FirmwareVersion new struct to compare version strings

--- Sources/Tools/Utils.swift
+++ Sources/Tools/Utils.swift
@@ -12,26 +12,3 @@ class ConversionUtils {
     return UInt8(v >> 8 )
   }
 }
-
-class ArrayUtils {
-
-  func compareArrayVersion(arrayA: [String],
-                           isGreaterThan arrayB: [String]) -> Int {
-    let coeffArrayA =
-      Int(arrayA[0])! * 10000 + Int(arrayA[1])! * 100 + Int(arrayA[2])!
-
-    let coeffArrayB =
-      Int(arrayB[0])! * 10000 + Int(arrayB[1])! * 100 + Int(arrayB[2])!
-
-    if coeffArrayA > coeffArrayB {
-      return 1
-    }
-
-    if coeffArrayA < coeffArrayB {
-      return -1
-    }
-
-    return 0
-
-  }
-}
