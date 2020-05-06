123c94fa78a1d4c0a31524e9dff546db9039a664
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-05-06 16:19:52 +0200

    Rename files

---
+++ Sources/Mailbox/MailBoxEvents.swift
@@ -0,0 +1,9 @@
+//
+//  MailBoxEvents.swift
+//  MyBrainTechnologiesSDK-iOS
+//
+//  Created by Mathilde Ressier on 06/05/2020.
+//  Copyright © 2020 MyBrainTechnologies. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/Mailbox/MailboxA2DPResponse.swift
@@ -0,0 +1,9 @@
+//
+//  MailboxA2DPResponse.swift
+//  MyBrainTechnologiesSDK-iOS
+//
+//  Created by Mathilde Ressier on 06/05/2020.
+//  Copyright © 2020 MyBrainTechnologies. All rights reserved.
+//
+
+import Foundation
