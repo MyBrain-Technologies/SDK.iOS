9e81206f13fe7266c5d18de4536718d3df9edb60
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-05-12 14:13:19 +0200

    Add comment on BrainwebRequestBuilder

--- Sources/Shared/Network/BrainwebRequestBuilder.swift
+++ Sources/Shared/Network/BrainwebRequestBuilder.swift
@@ -1,10 +1,26 @@
 import Foundation
 import Alamofire
 
+/*******************************************************************************
+ * BrainwebRequestBuilder
+ *
+ * Build a request object depending on the type of request expected
+ *
+ ******************************************************************************/
 struct BrainwebRequestBuilder {
 
-  private let path = "/ingest-legacy"
+  //----------------------------------------------------------------------------
+  // MARK: - Properties
+  //----------------------------------------------------------------------------
 
+  private let path = "/ingest-legacy"
+  private let timeout = TimeInterval(20)
+
+  //----------------------------------------------------------------------------
+  // MARK: - Methods
+  //----------------------------------------------------------------------------
+
+  /// Build a url request from a `baseURL`
   func build(fromBase baseURL: String) -> URLRequest? {
     guard var url = URL(string: baseURL) else { return nil }
 
@@ -13,12 +29,13 @@ struct BrainwebRequestBuilder {
     return URLRequest(url: url)
   }
 
+  /// Build a POST ingest request on the given brainweb url
   func buildIngestPost(fromBase baseURL: String,
                        token: String) -> URLRequest? {
     guard var urlRequest = build(fromBase: baseURL) else { return nil }
 
     urlRequest.httpMethod = HTTPMethod.post.rawValue
-    urlRequest.timeoutInterval = TimeInterval(20)
+    urlRequest.timeoutInterval = timeout
 
     urlRequest.setValue("Bearer " + token,
                         forHTTPHeaderField: "Authorization")
