1b684aa79f5cbf6c136dd46a4124098dc9f6d723
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-06-02 11:24:11 +0200

    WIP

--- MyBrainTechnologiesSDKTests/EEG/EEGAcquisitionBufferTests.swift
+++ MyBrainTechnologiesSDKTests/EEG/EEGAcquisitionBufferTests.swift
@@ -1,8 +1,65 @@
-  import XCTest
+import XCTest
+@testable import MyBrainTechnologiesSDK
 
-  class EEGAcquisitionBufferTests: XCTestCase {
+class EEGAcquisitionBufferTests: XCTestCase {
 
-    func testEEGAcquisitonBuffer() {
+  //----------------------------------------------------------------------------
+  // MARK: - Basic Packet
+  //----------------------------------------------------------------------------
+
+  func testVeryBasicPackets() {
+    let packets = EEGAcquisitonRawPacketsToBuffer.SetSample.packets
+    let expectedPackets = EEGAcquisitonRawPacketsToBuffer.SetSample.savedPackets
+
+    let acquisitionBuffer = EEGAcquisitionBuffer(bufferSizeMax: 3)
+
+    for (index, packet) in packets.enumerated() {
+      // When
+      acquisitionBuffer.add(rawPacketValue: packet)
+
+      // Then
+      let savedPackets = acquisitionBuffer.getUsablePackets()
+      XCTAssertEqual(savedPackets, expectedPackets[index])
     }
-
   }
+
+  func testVeryBasicPacketsBy6() {
+    let packets = EEGAcquisitonRawPacketsToBuffer.SetSample.packets
+    let expectedPackets =
+      EEGAcquisitonRawPacketsToBuffer.SetSample.savedPacketsBy6
+
+    let acquisitionBuffer = EEGAcquisitionBuffer(bufferSizeMax: 6)
+
+    for (index, packet) in packets.enumerated() {
+      // When
+      acquisitionBuffer.add(rawPacketValue: packet)
+
+      // Then
+      let savedPackets = acquisitionBuffer.getUsablePackets()
+      XCTAssertEqual(savedPackets, expectedPackets[index])
+    }
+  }
+
+  func testVeryBasicPacketsBy4() {
+    let packets = EEGAcquisitonRawPacketsToBuffer.SetSample.packets
+    let expectedPackets =
+      EEGAcquisitonRawPacketsToBuffer.SetSample.savedPacketsBy4
+
+    let acquisitionBuffer = EEGAcquisitionBuffer(bufferSizeMax: 4)
+
+    for (index, packet) in packets.enumerated() {
+      // When
+      acquisitionBuffer.add(rawPacketValue: packet)
+
+      // Then
+      let savedPackets = acquisitionBuffer.getUsablePackets()
+      XCTAssertEqual(savedPackets, expectedPackets[index])
+    }
+  }
+
+  //----------------------------------------------------------------------------
+  // MARK: - Basic Packets with lost packets
+  //----------------------------------------------------------------------------
+
+  
+}
--- Sources/EEG Module/EEGAcquisitionBuffer.swift
+++ Sources/EEG Module/EEGAcquisitionBuffer.swift
@@ -40,19 +40,28 @@ class EEGAcquisitionBuffer {
     guard data.count > 0 else { return }
 
     let packetValue = EEGRawPacket(data: data)
-    let currentIndex = packetValue.packetIndex
+    add(rawPacket: packetValue)
+  }
+
+  func add(rawPacketValue: [UInt8]) {
+    let packetValue = EEGRawPacket(rawValue: rawPacketValue)
+    add(rawPacket: packetValue)
+  }
+
+  func add(rawPacket: EEGRawPacket) {
+    let currentIndex = rawPacket.packetIndex
 
     let missingPackets = Int32(currentIndex - previousIndex)
 
     if missingPackets > 1 {
       log.info("Lost \(missingPackets) packets")
       packetBuffer.add(value: 0xFF,
-                       count: Int(missingPackets) * packetValue.valueLength)
+                       count: Int(missingPackets) * rawPacket.valueLength)
     }
 
-    previousIndex = currentIndex // SHOULD BE CLAMPED
+    previousIndex = currentIndex // SHOULD BE CLAMPED between 0 and Int16.max
 
-    packetBuffer.add(bytes: packetValue.value)
+    packetBuffer.add(bytes: rawPacket.value)
   }
 
   /// Return packets that can be used if the buffer is full, else nil if the packet is not full yet.
