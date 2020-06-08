97c9cd1c31d9aba1e947eab2a47626610772451d
Mathilde RESSIER <mathilde.ressier@mybraintech.com>
2020-06-08 10:46:53 +0200

    Remove old code

--- Sources/EEG Module/Signal Processing/EEGQualityProcessor.swift
+++ Sources/EEG Module/Signal Processing/EEGQualityProcessor.swift
@@ -7,21 +7,6 @@ struct EEGQualityProcessor {
                            sampRate: Int,
                            packetLength: Int,
                            nbChannel: Int) -> [Float] {
-    // Transform the input data into the format needed by the Obj-C++ bridge.
-//    var dataArray = [Float]()
-//    var nbNAN = 0
-
-//    for channelDatas in data {
-//      dataArray.append(contentsOf: channelDatas.values)
-//      for channelData in channelDatas.values {
-//        if channelData.isNaN {
-//          nbNAN += 1
-//        }
-//
-//        dataArray.append(channelData)
-//      }
-//    }
-
     let dataArray = Array(data.map() { $0.values }.joined())
 
     log.verbose("Compute quality value. Number of NaN values",
