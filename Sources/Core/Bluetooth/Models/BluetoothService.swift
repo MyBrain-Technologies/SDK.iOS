a4533df820829049488fc3f4714dca18ec22be70
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-04-11 16:59:01 +0200

    Add indus5 BluetoothService

--- Sources/Core/Bluetooth/Models/BluetoothService.swift
+++ Sources/Core/Bluetooth/Models/BluetoothService.swift
@@ -16,6 +16,11 @@ enum BluetoothService: String, CaseIterable {
   case audioSing = "0x110B"
   case remoteControl = "0x110C"
 
+  // Indus5
+  case transparentService_i5 = "49535343-FE7D-4AE5-8FA9-9FAFD205E455"
+  case rx_i5 = "49535343-1E4D-4BD9-BA61-23C647249616"
+  case tx_i5 = "49535343-8841-43F4-A8D4-ECBE34729BB3"
+
   //----------------------------------------------------------------------------
   // MARK: - Properties
   //----------------------------------------------------------------------------
