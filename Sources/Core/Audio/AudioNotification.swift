baf2e2706a46d6e2b7922c005c75dc1ef12b4007
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-04-14 12:10:24 +0200

    Mark classes as good

--- Sources/Core/Audio/AudioNotification.swift
+++ Sources/Core/Audio/AudioNotification.swift
@@ -7,6 +7,7 @@ import AVFoundation
  * Interpret notification on the audio scope.
  *
  ******************************************************************************/
+// Good
 struct AudioNotification {
 
   //----------------------------------------------------------------------------
