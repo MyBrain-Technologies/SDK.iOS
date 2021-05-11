86e5133c65a15666370014939791add490434a7e
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-05-11 10:14:45 +0200

    Update mock viewController

--- BluetoothTestApp/ViewController.swift
+++ BluetoothTestApp/ViewController.swift
@@ -9,6 +9,79 @@
 import UIKit
 import CoreBluetoothMock
 
+extension CBUUID {
+  static let nordicBlinkyService  = CBUUID(string: "00001523-1212-EFDE-1523-785FEABCD123")
+  static let buttonCharacteristic = CBUUID(string: "00001524-1212-EFDE-1523-785FEABCD123")
+  static let ledCharacteristic    = CBUUID(string: "00001525-1212-EFDE-1523-785FEABCD123")
+}
+
+extension CBMCharacteristicMock {
+
+  static let buttonCharacteristic = CBMCharacteristicMock(
+    type: .buttonCharacteristic,
+    properties: [.notify, .read],
+    descriptors: CBMClientCharacteristicConfigurationDescriptorMock()
+  )
+
+  static let ledCharacteristic = CBMCharacteristicMock(
+    type: .ledCharacteristic,
+    properties: [.write, .read]
+  )
+
+}
+
+class BlinkyCBMPeripheralSpecDelegate: CBMPeripheralSpecDelegate {
+  enum BlinkyCBMPeripheralSpecDelegateError: Error {
+    case connectionFailed
+  }
+
+  private var ledEnabled: Bool = false
+  private var buttonPressed: Bool = false
+
+  private var ledData: Data {
+    return ledEnabled ? Data([0x01]) : Data([0x00])
+  }
+
+  private var buttonData: Data {
+    return buttonPressed ? Data([0x01]) : Data([0x00])
+  }
+
+  func peripheral(_ peripheral: CBMPeripheralSpec,
+                  didReceiveReadRequestFor characteristic: CBMCharacteristic)
+  -> Result<Data, Error> {
+    if characteristic.uuid == .ledCharacteristic {
+      return .success(ledData)
+    } else {
+      return .success(buttonData)
+    }
+  }
+
+  func peripheral(_ peripheral: CBMPeripheralSpec,
+                  didReceiveWriteRequestFor characteristic: CBMCharacteristic,
+                  data: Data) -> Result<Void, Error> {
+    if data.count > 0 {
+      ledEnabled = data[0] != 0x00
+    }
+    return .success(())
+  }
+
+  func peripheral(_ peripheral: CBMPeripheralSpec,
+                  didReceiveServiceDiscoveryRequest serviceUUIDs: [CBMUUID]?
+  ) -> Result<Void, Error> {
+    return .success(())
+  }
+
+  func peripheral(_ peripheral: CBMPeripheralSpec, didDisconnect error: Error?) {
+    print("Disconnected")
+  }
+
+  func peripheralDidReceiveConnectionRequest(_ peripheral: CBMPeripheralSpec) -> Result<Void, Error> {
+//    print("Connection")
+    return .success(())
+  }
+
+}
+
 class ViewController: UIViewController {
 
   //----------------------------------------------------------------------------
@@ -21,6 +94,8 @@ class ViewController: UIViewController {
   /******************** Bluetooth ********************/
 
   var bluetoothMock: CBCentralManager?
+
+  let mock = CBMCentralManagerMock()
 
   //----------------------------------------------------------------------------
   // MARK: - Lifecycle
@@ -44,13 +119,43 @@ class ViewController: UIViewController {
     powerSwitch.isOn = false
   }
 
+  lazy var blinky: CBMPeripheralSpec = {
+    let service = CBMServiceMock(type: CBUUID.nordicBlinkyService, primary: true)
+
+    return CBMPeripheralSpec
+      .simulatePeripheral(proximity: .immediate)
+      .advertising(
+        advertisementData: [
+          CBAdvertisementDataLocalNameKey    : "nRF Blinky",
+          CBAdvertisementDataServiceUUIDsKey : [CBUUID.nordicBlinkyService],
+          CBAdvertisementDataIsConnectable   : true as NSNumber
+        ],
+        withInterval: 0.250,
+        alsoWhenConnected: false)
+      .connectable(
+        name: "nRF Blinky",
+        services: [service],
+        delegate: BlinkyCBMPeripheralSpecDelegate(),
+        connectionInterval: 0.150,
+        mtu: 23)
+      .build()
+  }()
+
   private func setupMockBluetooth() {
 
+
+
+
+
+
+
+    CBMCentralManagerMock.simulatePeripherals([blinky])
 
     bluetoothMock = CBCentralManagerFactory.instance(delegate: self,
                                                      queue: nil,
                                                      forceMock: true)
-//    bluetoothMock?.scanForPeripherals(withServices: nil, options: nil)
+
+    CBMCentralManagerMock.simulatePowerOn()
   }
 
   //----------------------------------------------------------------------------
@@ -88,17 +193,21 @@ extension ViewController: CBCentralManagerDelegate {
     switch central.state {
       case .poweredOn:
         print("On")
+        bluetoothMock?.scanForPeripherals(
+          withServices: [CBUUID.nordicBlinkyService],
+          options: nil
+        )
       case .poweredOff:
         print("Off")
       default: break
     }
 
-//    let hasRebootBluetooth = bluetoothStatesHistory.isPoweredOn
-//      && bluetoothStatesHistory.historyIsFull
-//
-//    if isOADInProgress && OADState == .rebootRequired && hasRebootBluetooth {
-//      continueOADAfterBluetoothReboot()
-//    }
+    //    let hasRebootBluetooth = bluetoothStatesHistory.isPoweredOn
+    //      && bluetoothStatesHistory.historyIsFull
+    //
+    //    if isOADInProgress && OADState == .rebootRequired && hasRebootBluetooth {
+    //      continueOADAfterBluetoothReboot()
+    //    }
   }
 
   /// Check out the discovered peripherals to find the right device.
@@ -107,54 +216,60 @@ extension ViewController: CBCentralManagerDelegate {
                       didDiscover peripheral: CBPeripheral,
                       advertisementData: [String: Any],
                       rssi RSSI: NSNumber) {
-    print("🆕 Did discover peripheral")
+    print("🆕 Did discover peripheral \(peripheral)")
 
-//    let dataReader = BluetoothAdvertisementDataReader(data: advertisementData)
-//
-//    guard let newDeviceName = dataReader.localName,
-//      let newDeviceServices = dataReader.uuidKeys else { return }
-//
-//    let isMelomindDevice = MelomindBluetoothPeripheral.isMelomindDevice(
-//      deviceName: newDeviceName,
-//      services: newDeviceServices
-//    )
-//    let isConnectingOrUpdating =
-//      timers.isBleConnectionTimerInProgress || OADState >= .started
-//
-//    guard isMelomindDevice && isConnectingOrUpdating else { return }
-//
-//    if DeviceManager.connectedDeviceName == "" {
-//      DeviceManager.connectedDeviceName = newDeviceName
-//    }
-//
-//    guard DeviceManager.connectedDeviceName == newDeviceName else { return }
-//
-//    bluetoothConnector.stopScanningForConnections()
-//    peripheralIO.peripheral = peripheral
-//
-//    peripheralIO.peripheral?.delegate = self
-//
-//    bluetoothConnector.connect(to: peripheral)
-//
-//    DeviceManager.updateDeviceToMelomind()
+
+    bluetoothMock?.stopScan()
+    central.connect(peripheral, options: nil)
+
+    //    let dataReader = BluetoothAdvertisementDataReader(data: advertisementData)
+    //
+    //    guard let newDeviceName = dataReader.localName,
+    //      let newDeviceServices = dataReader.uuidKeys else { return }
+    //
+    //    let isMelomindDevice = MelomindBluetoothPeripheral.isMelomindDevice(
+    //      deviceName: newDeviceName,
+    //      services: newDeviceServices
+    //    )
+    //    let isConnectingOrUpdating =
+    //      timers.isBleConnectionTimerInProgress || OADState >= .started
+    //
+    //    guard isMelomindDevice && isConnectingOrUpdating else { return }
+    //
+    //    if DeviceManager.connectedDeviceName == "" {
+    //      DeviceManager.connectedDeviceName = newDeviceName
+    //    }
+    //
+    //    guard DeviceManager.connectedDeviceName == newDeviceName else { return }
+    //
+    //    bluetoothConnector.stopScanningForConnections()
+    //    peripheralIO.peripheral = peripheral
+    //
+    //    peripheralIO.peripheral?.delegate = self
+    //
+    //    bluetoothConnector.connect(to: peripheral)
+    //
+    //    DeviceManager.updateDeviceToMelomind()
   }
 
   // Called when it succeeded
-  func centralManager(central: CBCentralManager,
-                      didConnectPeripheral peripheral: CBPeripheral) {
+  func centralManager(_ central: CBMCentralManager,
+                      didConnect peripheral: CBMPeripheral) {
     print("🆕 Did connect to peripheral")
 
-//    didConnectToPeripheral?(peripheral)
+    peripheral.delegate = self
+    peripheral.discoverServices(nil)
+    //    didConnectToPeripheral?(peripheral)
 
 
 
-//    peripheral.discoverServices(nil) // Return all the possible services
-//
-//    guard isOADInProgress && OADState >= .completed else {
-//      return DeviceManager.resetDeviceInfo()
-//    }
-//
-//    bluetoothDeviceCharacteristics.deviceInformations.removeAll()
+    //    peripheral.discoverServices(nil) // Return all the possible services
+    //
+    //    guard isOADInProgress && OADState >= .completed else {
+    //      return DeviceManager.resetDeviceInfo()
+    //    }
+    //
+    //    bluetoothDeviceCharacteristics.deviceInformations.removeAll()
   }
 
   // Called when it failed
@@ -162,8 +277,8 @@ extension ViewController: CBCentralManagerDelegate {
                       didFailToConnect peripheral: CBPeripheral,
                       error: Error?) {
     print("🆕 Did fail to connect to peripheral: \(peripheral)")
-//    eventDelegate?.onConnectionFailed?(error)
-//    didConnectionFail?(error)
+    //    eventDelegate?.onConnectionFailed?(error)
+    //    didConnectionFail?(error)
   }
 
   /// If disconnected by error, start searching again,
@@ -173,13 +288,27 @@ extension ViewController: CBCentralManagerDelegate {
                       error: Error?) {
     print("🆕 Did disconnect peripheral \(peripheral)")
 
-//    processBatteryLevel = false
-//    if isOADInProgress {
-//      peripheralDisconnectedDuringOAD()
-//    } else {
-//      peripheralDisconnected(error: error)
-//    }
+    //    processBatteryLevel = false
+    //    if isOADInProgress {
+    //      peripheralDisconnectedDuringOAD()
+    //    } else {
+    //      peripheralDisconnected(error: error)
+    //    }
   }
 
 
+}
+
+
+extension ViewController: CBPeripheralDelegate {
+
+  func peripheral(_ peripheral: CBMPeripheral,
+                  didDiscoverServices error: Error?) {
+    print(peripheral.services)
+    if let services = peripheral.services,
+       let service = services.first(where: { $0.uuid == CBUUID.nordicBlinkyService })  {
+      print("Good \(service)")
+    }
+    print("End didDiscoverServices")
+  }
 }
