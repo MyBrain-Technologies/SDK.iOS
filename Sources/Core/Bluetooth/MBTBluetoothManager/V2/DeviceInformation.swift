a75da836e70d8bc2f86a42dc3bf566f596353a2e
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-05-27 09:57:00 +0200

    Make DeviceInformation codable

--- Sources/Core/Bluetooth/MBTBluetoothManager/V2/DeviceInformation.swift
+++ Sources/Core/Bluetooth/MBTBluetoothManager/V2/DeviceInformation.swift
@@ -1,48 +1,29 @@
 import Foundation
 
-public struct DeviceInformation {
+public struct DeviceInformation: Codable {
 
   //----------------------------------------------------------------------------
   // MARK: - Properties
   //----------------------------------------------------------------------------
 
   /// The commercial name of the device.
-  public var productName: String?
+  public var productName: String
 
   /// The product specific serial number.
-  public var deviceId: String?
+  public var deviceId: String
 
   /// The product hardware version.
-  public var hardwareVersion: String?
+  public var hardwareVersion: String
 
   /// The product firmware version.
-  public var firmwareVersion: String?
-
-  /******************** Computed properties ********************/
+  public var firmwareVersion: String
 
   var indusVersion: IndusVersion? {
-    guard let hardwareVersion = hardwareVersion else { return nil }
     return IndusVersion(fromHardwareVersion: hardwareVersion)
   }
 
-  /// Allows to know if all properties have been initialized
-  ///
-  /// - Returns: A *Bool* instance which test if one of the four properties is
-  /// nil
-  var isDeviceInfoNotNil: Bool {
-    return productName != nil
-      && deviceId != nil
-      && hardwareVersion != nil
-      && firmwareVersion != nil
-  }
-
-  /******************** Conversion ********************/
-
-  var melomindDeviceInformation: MelomindDeviceInformations {
-    return MelomindDeviceInformations(productName: productName ?? "",
-                                      hardwareVersion: hardwareVersion ?? "",
-                                      firmwareVersion: firmwareVersion ?? "",
-                                      uniqueDeviceIdentifier: deviceId ?? "")
+  var formattedFirmwareVersion: FormatedVersion {
+    return FormatedVersion(string: firmwareVersion)
   }
 
 }
