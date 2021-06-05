e3853777b2c367b1cba0067d696fefdae9c747bd
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-06-05 15:38:21 +0200

    Improve buffer type

--- Sources/Core/Bluetooth/MBTBluetoothManager/V2/Acquisition/Buffer.swift
+++ Sources/Core/Bluetooth/MBTBluetoothManager/V2/Acquisition/Buffer.swift
@@ -8,4 +8,13 @@ public extension Buffer {
     return self.flatMap { $0 }
   }
 
+  var channelCount: Int {
+    return self.count
+  }
+
+  func channel(at index: Int) -> [Float]? {
+    guard index < channelCount else { return nil }
+    return self[index]
+  }
+
 }
