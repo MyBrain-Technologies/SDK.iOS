542507620b40945c33c5d3d269864d22665cc4fe
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-06-09 12:32:33 +0200

    Make IndusVersion codable

--- Sources/Shared/Models/Versioning/IndusVersion.swift
+++ Sources/Shared/Models/Versioning/IndusVersion.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public enum IndusVersion: CaseIterable {
+public enum IndusVersion: CaseIterable, Codable {
 
   //----------------------------------------------------------------------------
   // MARK: - Cases
