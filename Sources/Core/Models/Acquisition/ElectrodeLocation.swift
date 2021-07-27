dcd75662afb716e581da5255b4e1b317253a1e6b
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-07-27 18:15:39 +0200

    Enable string rawvalue for ElectrodeLocation

--- Sources/Core/Models/Acquisition/ElectrodeLocation.swift
+++ Sources/Core/Models/Acquisition/ElectrodeLocation.swift
@@ -1,202 +1,101 @@
 import Foundation
 
 /// Enum of differents electrodes possible locations.
-public enum ElectrodeLocation: Int, Codable {
-    case fpz
-    case fp1
-    case fp2
+public enum ElectrodeLocation: String, Codable {
+  case fpz = "Fpz"
+  case fp1 = "Fp1"
+  case fp2 = "Fp2"
 
-    case af7
-    case af3
-    case afz
-    case af4
-    case ad8
+  case af7 = "AF7"
+  case af3 = "AF3"
+  case afz = "AFz"
+  case af4 = "AF4"
+  case ad8 = "AD8"
 
-    case f9
-    case f7
-    case f5
-    case f3
-    case f1
-    case fz
-    case f2
-    case f4
-    case f6
-    case f8
-    case f10
+  case f9 = "F9"
+  case f7 = "F7"
+  case f5 = "F5"
+  case f3 = "F3"
+  case f1 = "F1"
+  case fz = "Fz"
+  case f2 = "F2"
+  case f4 = "F4"
+  case f6 = "F6"
+  case f8 = "F8"
+  case f10 = "F10"
 
-    case ft9
-    case ft7
-    case fc5
-    case fc3
-    case fc1
-    case fcz
-    case fc2
-    case fc4
-    case fc6
-    case ft8
-    case ft10
+  case ft9 = "FT9"
+  case ft7 = "FT7"
+  case fc5 = "FC5"
+  case fc3 = "FC3"
+  case fc1 = "FC1"
+  case fcz = "FCz"
+  case fc2 = "FC2"
+  case fc4 = "FC4"
+  case fc6 = "FC6"
+  case ft8 = "FT8"
+  case ft10 = "FT10"
 
-    case t7
-    case c5
-    case c3
-    case c1
-    case cz
-    case c2
-    case c4
-    case c6
-    case t8
+  case t7 = "T7"
+  case c5 = "C5"
+  case c3 = "C3"
+  case c1 = "C1"
+  case cz = "Cz"
+  case c2 = "C2"
+  case c4 = "C4"
+  case c6 = "C6"
+  case t8 = "T8"
 
-    case tp9
-    case tp7
-    case cp5
-    case cp3
-    case cp1
-    case cpz
-    case cp2
-    case cp4
-    case cp6
-    case tp8
-    case tp10
+  case tp9 = "TP9"
+  case tp7 = "TP7"
+  case cp5 = "CP5"
+  case cp3 = "CP3"
+  case cp1 = "CP1"
+  case cpz = "CPz"
+  case cp2 = "CP2"
+  case cp4 = "CP4"
+  case cp6 = "CP6"
+  case tp8 = "TP8"
+  case tp10 = "TP10"
 
-    case p9
-    case p7
-    case p5
-    case p3
-    case p1
-    case pz
-    case p2
-    case p4
-    case p6
-    case p8
-    case p10
+  case p9 = "P9"
+  case p7 = "P7"
+  case p5 = "P5"
+  case p3 = "P3"
+  case p1 = "P1"
+  case pz = "Pz"
+  case p2 = "P2"
+  case p4 = "P4"
+  case p6 = "P6"
+  case p8 = "P8"
+  case p10 = "P10"
 
-    case po3
-    case poz
-    case po4
+  case po3 = "PO3"
+  case poz = "POz"
+  case po4 = "PO4"
 
-    case po7
-    case o1
-    case oz
-    case o2
-    case po8
+  case po7 = "PO7"
+  case o1 = "O1"
+  case oz = "Oz"
+  case o2 = "O2"
+  case po8 = "PO8"
 
-    case po9
-    case o9
-    case iz
-    case o10
-    case po10
+  case po9 = "PO9"
+  case o9 = "O9"
+  case iz = "Iz"
+  case o10 = "O10"
+  case po10 = "PO10"
 
-    case m1 // Mastoid 1
-    case m2  // Mastoid 2
+  case m1 = "M1" // Mastoid 1
+  case m2 = "M2"  // Mastoid 2
 
-    case acc
+  case acc = "ACC"
 
-    case ext1
-    case ext2
-    case ext3
+  case ext1 = "EXT1"
+  case ext2 = "EXT2"
+  case ext3 = "EXT3"
 
-    case null1
-    case null2
-    case null3
-
-    var stringValue: String {
-        switch self {
-        case .fpz: return "Fpz"
-        case .fp1: return "Fp1"
-        case .fp2: return "Fp2"
-
-        case .af7: return "AF7"
-        case .af3: return "AF3"
-        case .afz: return "AFz"
-        case .af4: return "AF4"
-        case .ad8: return "AD8"
-
-        case .f9: return "F9"
-        case .f7: return "F7"
-        case .f5: return "F5"
-        case .f3: return "F3"
-        case .f1: return "F1"
-        case .fz: return "Fz"
-        case .f2: return "F2"
-        case .f4: return "F4"
-        case .f6: return "F6"
-        case .f8: return "F8"
-        case .f10: return "F10"
-
-        case .ft9: return "FT9"
-        case .ft7: return "FT7"
-        case .fc5: return "FC5"
-        case .fc3: return "FC3"
-        case .fc1: return "FC1"
-        case .fcz: return "FCz"
-        case .fc2: return "FC2"
-        case .fc4: return "FC4"
-        case .fc6: return "FC6"
-        case .ft8: return "FT8"
-        case .ft10: return "FT10"
-
-        case .t7: return "T7"
-        case .c5: return "C5"
-        case .c3: return "C3"
-        case .c1: return "C1"
-        case .cz: return "Cz"
-        case .c2: return "C2"
-        case .c4: return "C4"
-        case .c6: return "C6"
-        case .t8: return "T8"
-
-        case .tp9: return "TP9"
-        case .tp7: return "TP7"
-        case .cp5: return "CP5"
-        case .cp3: return "CP3"
-        case .cp1: return "CP1"
-        case .cpz: return "CPz"
-        case .cp2: return "CP2"
-        case .cp4: return "CP4"
-        case .cp6: return "CP6"
-        case .tp8: return "TP8"
-        case .tp10: return "TP10"
-
-        case .p9: return "P9"
-        case .p7: return "P7"
-        case .p5: return "P5"
-        case .p3: return "P3"
-        case .p1: return "P1"
-        case .pz: return "Pz"
-        case .p2: return "P2"
-        case .p4: return "P4"
-        case .p6: return "P6"
-        case .p8: return "P8"
-        case .p10: return "P10"
-
-        case .po3: return "PO3"
-        case .poz: return "POz"
-        case .po4: return "PO4"
-
-        case .po7: return "PO7"
-        case .o1: return "O1"
-        case .oz: return "Oz"
-        case .o2: return "O2"
-        case .po8: return "PO8"
-
-        case .po9: return "PO9"
-        case .o9: return "O9"
-        case .iz: return "Iz"
-        case .o10: return "O10"
-        case .po10: return "PO10"
-
-        case .m1: return "M1" // Mastoid 1
-        case .m2: return "M2"  // Mastoid 2
-
-        case .acc: return "ACC"
-
-        case .ext1: return "EXT1"
-        case .ext2: return "EXT2"
-        case .ext3: return "EXT3"
-
-        case .null1: return "NULL1"
-        case .null2: return "NULL2"
-        case .null3: return "NULL3"
-        }
-    }
+  case null1 = "NULL1"
+  case null2 = "NULL2"
+  case null3 = "NULL3"
 }
