92b114795d667824d4a6cc21f19a3cc3b786c01d
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-08-06 19:51:04 +0200

    Add MbtImsPacket and ImsAcquisitionProcessor files

--- Sources/Core/EEG Module/Acquisition/ImsAcquisitionProcessor.swift
+++ Sources/Core/EEG Module/Acquisition/ImsAcquisitionProcessor.swift
@@ -1,1 +1,44 @@
 import Foundation
+
+class ImsAcquisitionProcessor {
+
+  //----------------------------------------------------------------------------
+  // MARK: - Properties
+  //----------------------------------------------------------------------------
+
+  private let packetLength: Int
+
+  private let channelCount: Int
+
+  private let sampletRate: Int
+
+  //----------------------------------------------------------------------------
+  // MARK: - Initialization
+  //----------------------------------------------------------------------------
+
+  init(packetLength: Int, channelCount: Int = 3, sampletRate: Int) {
+    self.packetLength = packetLength
+    self.channelCount = channelCount
+    self.sampletRate = sampletRate
+  }
+
+
+  //----------------------------------------------------------------------------
+  // MARK: - Processes
+  //----------------------------------------------------------------------------
+
+//  func generateEmsPacket(fromData data: Data) -> MbtImsPacket {
+////    acquisitionBuffer.add(data: data)
+////    guard let packet = acquisitionBuffer.getUsablePackets() else { return nil }
+////
+////    let relaxIndexes =
+////      EEGDeserializer.deserializeToRelaxIndex(bytes: packet,
+////                                              numberOfElectrodes: channelCount)
+////    let eegPacket = convertToEEGPacket(values: relaxIndexes,
+////                                       hasQualityChecker: hasQualityChecker)
+////
+////    return eegPacket
+//  }
+
+
+}
