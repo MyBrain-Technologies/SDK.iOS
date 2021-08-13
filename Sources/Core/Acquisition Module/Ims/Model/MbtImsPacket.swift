2b1e722d2d6f6c44297485c1ad9d54e1bc8bd4f7
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-08-13 23:42:41 +0200

    Add getter to MbtImsPacket

--- Sources/Core/Acquisition Module/Ims/Model/MbtImsPacket.swift
+++ Sources/Core/Acquisition Module/Ims/Model/MbtImsPacket.swift
@@ -2,21 +2,35 @@ import Foundation
 
 public class MbtImsPacket {
 
+  typealias Coordinate = (x: Float, y: Float, z: Float)
+
   //----------------------------------------------------------------------------
   // MARK: - Properties
   //----------------------------------------------------------------------------
 
+  /******************** Coordinate ********************/
+
+  let x: Float
+
+  let y: Float
+
+  let z: Float
+
+  let coordinate: Coordinate
 
   //----------------------------------------------------------------------------
   // MARK: - Initialization
   //----------------------------------------------------------------------------
 
-  public init(with buffer: Buffer) {
-
-  }
+//  public init(with buffer: Buffer) {
+//
+//  }
 
   init(x: Float, y: Float, z: Float) {
-
+    self.x = x
+    self.y = y
+    self.z = z
+    self.coordinate = (x, y, z)
   }
 
 }
