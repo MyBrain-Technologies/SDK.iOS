b4dcd6598cc1f999d09fcbc9939f5557a34e7a82
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-08-13 14:52:12 +0200

    Add ImsAcquiser file

---
+++ Sources/Core/Acquisition Module/Ims/ImsAcquiser.swift
@@ -0,0 +1,9 @@
+//
+//  ImsAcquiser.swift
+//  MyBrainTechnologiesSDK
+//
+//  Created by Laurent on 13/08/2021.
+//  Copyright © 2021 MyBrainTechnologies. All rights reserved.
+//
+
+import Foundation
