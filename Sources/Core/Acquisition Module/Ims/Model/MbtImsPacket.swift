3c1cc5439d240956080c6c0f0ad62be2992b5f83
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-08-16 11:10:41 +0200

    Improve MbtImsPacket

--- Sources/Core/Acquisition Module/Ims/Model/MbtImsPacket.swift
+++ Sources/Core/Acquisition Module/Ims/Model/MbtImsPacket.swift
@@ -2,35 +2,38 @@ import Foundation
 
 public class MbtImsPacket {
 
-  typealias Coordinate = (x: Float, y: Float, z: Float)
+  public typealias Coordinate = (x: Float, y: Float, z: Float)
 
   //----------------------------------------------------------------------------
   // MARK: - Properties
   //----------------------------------------------------------------------------
 
+  /// The timestamp in milliseconds when this packet is created.
+  public let timestamp = Int(Date().timeIntervalSince1970 * 1000)
+
   /******************** Coordinate ********************/
 
-  let x: Float
-
-  let y: Float
-
-  let z: Float
-
-  let coordinate: Coordinate
+  public let coordinates: [Coordinate]
 
   //----------------------------------------------------------------------------
   // MARK: - Initialization
   //----------------------------------------------------------------------------
 
-//  public init(with buffer: Buffer) {
-//
-//  }
+  public init?(with rawCordinates: [[Float]]) {
+    var coordinates = [Coordinate]()
+    for rawCoordinate in rawCordinates {
+      guard rawCoordinate.count == 3 else { return nil }
+      let x = rawCoordinate[0]
+      let y = rawCoordinate[1]
+      let z = rawCoordinate[2]
+      let coordinate = Coordinate(x, y, z)
+      coordinates.append(coordinate)
+    }
+    self.coordinates = coordinates
+  }
 
-  init(x: Float, y: Float, z: Float) {
-    self.x = x
-    self.y = y
-    self.z = z
-    self.coordinate = (x, y, z)
+  public init(with coordinates: [Coordinate]) {
+    self.coordinates = coordinates
   }
 
 }
